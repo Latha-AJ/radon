f9f6d434325e687c7bd7bf262153cdbd2533c759
Latha-AJ <lathajappa.7226@gmail.com>
2022-06-15 21:57:47 +0530

    commiting the authorisation assignment

--- src/middleWares/auth.js
+++ src/middleWares/auth.js
@@ -12,11 +12,19 @@ const auth = async function (req, res, next) {
     let decodedToken = jwt.verify(token, "functionup-radon");
   if (!decodedToken)
     return res.send({ status: false, msg: "token is invalid" });
+    
+//userId comparision to check if the logged-in user is requesting for their own data    
+    let userToBeModified = req.params.userId
+    let userLoggedIn = decodedToken.userId
+ if(userToBeModified != userLoggedIn) return res.send({status: false, msg: 'User logged is not allowed to modify the requested users data'})
+
+// checking if the userID present in the database
 
     let userId = req.params.userId;
     let userDetails = await userModel.findById(userId);
     if (!userDetails)
       return res.send({ status: false, msg: "No such user exists" });
+
          next();
 
     // res.send({ status: true, data: userDetails });
